e1e27023e6d513d157cc358a3d774a0f33881e2d
Kiran Gill <kiran.stefan.gill@gmail.com>
2019-02-07 12:23:02 +0100

    extra_Task_Bank

--- src/app/bank.py
+++ src/app/bank.py
@@ -14,8 +14,12 @@ class Bank:
         return account
 
     def add_transaction(self, *, sender, recipient, subject, amount):
-        self.transactions.append(app.Transaction(sender=sender.number, recipient=recipient.number, subject=subject,
-                                                 amount=amount))
         assert sender.number in self.accounts, 'Sender has no account yet!'
         assert recipient.number in self.accounts, 'Recipient has no account yet!'
+        assert sender.has_funds_for(amount), 'Account has not enough funds'
+
+        self.transactions.append(app.Transaction(sender=sender.number, recipient=recipient.number, subject=subject,
+                                                 amount=amount))
+        sender.subtract_from_balance(amount)
+        recipient.add_to_balance(amount)
         return self.transactions[-1]
